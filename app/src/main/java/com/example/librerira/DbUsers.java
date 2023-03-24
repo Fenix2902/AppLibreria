4d8eb419fce4cb2c2b6de1306b75c9ef808da80d
Fenix2902 <alexandercalleagudelo@gmail.com>
2023-03-24 10:31:07 -0500

    se inicia la configuracion de botones crear en MainActivity de libros

--- app/src/main/java/com/example/librerira/DbUsers.java
+++ app/src/main/java/com/example/librerira/DbUsers.java
@@ -8,7 +8,8 @@ public class DbUsers extends SQLiteOpenHelper {
     //creacion de tablas
 
     String tbUser = "Create Table Users(idUser text primary key, name text,email text, password text, status integer)";
-   /* String tbRent = "Create Table Rent(idRent text primary key, idUser text, idBook text, date date)";*/
+   /* String tbBook = "Create Table Books(idbook text primary key, name text, coste text, available integer)";
+    String tbRent = "Create Table Rent(idRent text primary key, idUser text, idBook text, date date)";*/
 
     public DbUsers(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
@@ -18,16 +19,19 @@ public class DbUsers extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase Db) {
         //ingresar tablas
         Db.execSQL(tbUser);
-        /*Db.execSQL(tbRent);*/
+        /*Db.execSQL(tbBook);
+        Db.execSQL(tbRent);*/
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase Db, int oldVersion, int newVersion) {
         Db.execSQL("Drop Table Users");
-        /*Db.execSQL("Drop Table Rent");*///borra el dato anterior
+        //Db.execSQL("Drop Table Rent");//borra el dato anterior
 
+        //Db.execSQL("Drop table Books");
+        //Db.execSQL(tbBook);//actualiza
         Db.execSQL(tbUser);//actualiza//
-        /*Db.execSQL(tbRent);*/
+        //Db.execSQL(tbRent);
     }
 }
--- app/src/main/java/com/example/librerira/MainActivity_books.java
+++ app/src/main/java/com/example/librerira/MainActivity_books.java
@@ -1,6 +1,7 @@
 package com.example.librerira;
 
 import android.content.ContentValues;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
@@ -11,7 +12,6 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity_books extends AppCompatActivity {
@@ -32,43 +32,45 @@ public class MainActivity_books extends AppCompatActivity {
 
         //referenciar elementos instanciados
 
-        //idlibro.findViewById(R.id.etidlibro);
-        /*libro.findViewById(R.id.etlibro);
-        costo.findViewById(R.id.etcosto);*/
+        idlibro =findViewById(R.id.teidlibro);
+        libro = findViewById(R.id.etnomlibro);
+        costo = findViewById(R.id.etcosto);
 
-        /*disponible.findViewById(R.id.rbdisponible);
-        nodisponible.findViewById(R.id.rbnodisponible);
+        disponible = findViewById(R.id.rbdisponible);
+        nodisponible = findViewById(R.id.rbnodisponible);
 
-        crear.findViewById(R.id.btncrear);
-        editar.findViewById(R.id.btneditar);
-        buscar.findViewById(R.id.btnbuscar);
-        borrar.findViewById(R.id.btnborrar);
-        listar.findViewById(R.id.btnlist);
-        listar.setEnabled(false);*/
+        crear = findViewById(R.id.btncrear);
+        editar = findViewById(R.id.btneditar);
+        buscar = findViewById(R.id.btnbuscar);
+        borrar = findViewById(R.id.btnborrar);
+        listar = findViewById(R.id.btnlist);
+        listar.setEnabled(false);
+
+       String oldidlibro = "";
 
         //evento de crear libros
 
-       /* crear.setOnClickListener(new View.OnClickListener() {
+        crear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (!idlibro.getText().toString().isEmpty() && !libro.getText().toString().isEmpty() && !costo.getText().toString().isEmpty()) {
                     //Buscar la identificacion en la DB
                     SQLiteDatabase userRead = books.getReadableDatabase();
                     String query = "Select idBook From Books Where idBook ='" + idlibro.getText().toString() + "'";
-                    Cursor cUser = userRead.rawQuery(query, null);
-                    if (!cUser.moveToFirst()) {//si no encuentra el id del usuario
+                    Cursor cBook = userRead.rawQuery(query, null);
+                    if (!cBook.moveToFirst()) {//si no encuentra el id del usuario
 
                         //Instanciar clase SQLiteDatabase como escritura
                         SQLiteDatabase sdUser = books.getWritableDatabase();
 
                         //Contenedor de Valores
 
-                        ContentValues cvUser = new ContentValues();
-                        cvUser.put("idbook", idlibro.getText().toString());
-                        cvUser.put("name", libro.getText().toString());
-                        cvUser.put("coste", costo.getText().toString());
-                        cvUser.put("status", disponible.isChecked() ? 1 : 0);
-                        sdUser.insert("Books", null, cvUser);
+                        ContentValues cvBook = new ContentValues();
+                        cvBook.put("idbook", idlibro.getText().toString());
+                        cvBook.put("name", libro.getText().toString());
+                        cvBook.put("coste", costo.getText().toString());
+                        cvBook.put("status", disponible.isChecked() ? 1 : 0);
+                        sdUser.insert("Books", null, cvBook);
                         sdUser.close();
                         //limpiar();
 
@@ -80,7 +82,83 @@ public class MainActivity_books extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), "Ingrese todos los datos del formulario..", Toast.LENGTH_LONG).show();
                 }
             }
-        });*/
+        });
+
+        editar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SQLiteDatabase sdBook = books.getWritableDatabase();
+                if (idlibro.getText().toString().equals(oldidlibro)){
+                    sdBook.execSQL("Update Books set name ='"+libro.getText().toString()+"',coste= '"+costo.getText().toString()+"',avalaible='"+(disponible.isChecked()? 1:0)+"'where idbook= '"+ oldidlibro +"'");
+                    Toast.makeText(getApplicationContext(), "Usuario actualizado correctamente", Toast.LENGTH_SHORT).show();
+                }else {
+                    SQLiteDatabase sdBookread = books.getReadableDatabase();
+                    String query = "Select idBook From Books Where idBook ='" + idlibro.getText().toString() + "'";
+                    Cursor cBook = sdBookread.rawQuery(query,null);
+                    if (!cBook.moveToFirst()){
+                        sdBook.execSQL("Update Books set name ='"+libro.getText().toString()+"',coste= '"+costo.getText().toString()+"',avalaible='"+(disponible.isChecked()? 1:0)+"'where idbook= '"+ oldidlibro +"'");
+                        Toast.makeText(getApplicationContext(), "Libro actualizado correctamente", Toast.LENGTH_SHORT).show();
+                    }else {
+                        Toast.makeText(getApplicationContext(),"Este Libro ya esta asignado a otro usuario, intente con otro email...", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
+
+        buscar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                searchBook(idlibro.getText().toString());
+            }
+
+            private Cursor findBook(String fidBook) {
+                SQLiteDatabase dbsearchBook = books.getReadableDatabase();
+                String sqlSearch = "Select idbook, name, coste, available From Books Where idbook ='"+fidBook+"'";
+                Cursor cBook = dbsearchBook.rawQuery(sqlSearch,null);
+                if (cBook.moveToFirst()){
+                    dbsearchBook.close();
+                    return cBook;
+                }else {
+                    dbsearchBook.close();
+                    return cBook;
+                }
+            }
+
+            private void searchBook(String sidBook) {
+                Cursor cBook =findBook(sidBook);
+                if (findBook(sidBook).moveToFirst()){
+                    //Mostrar los datos del usuario en pantalla
+                    //oldidlibro = idlibro.getText().toString();
+                    libro.setText(cBook.getString(1));
+                    costo.setText(cBook.getString(2));
+                    if (cBook.getInt(3)==1){
+                        disponible.setChecked(true);
+                        listar.setEnabled(true);
+                    }else {
+                        nodisponible.setChecked(true);
+                        listar.setEnabled(false);
+                    }
+                }else {
+                    Toast.makeText(getApplicationContext(), "El ID del usuario no fue encontrado", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        //Evento Click para Listado de Libros
+        listar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Verificar si el Libro esta disponible
+                if (disponible.isChecked()){
+                    //pasar a la actividad que muestra los libros
+                    startActivity(new Intent(getApplicationContext(),listLibros.class));//permite ir a otra pagina
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"El libro no esta disponible",Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
 
     }
 }
